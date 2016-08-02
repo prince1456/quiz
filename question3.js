b80e939a643c3627dcb15024e8c4743a78b871ad
admin <admin@INVOKE9.local>
2016-08-02 10:50:55 -0700

    question 3 javascript

---
+++ question3.js
@@ -0,0 +1,13 @@
+var majorCities = {BC: ["Vancouver", "Victoria", "Prince George"],
+ AB: ["Edmonton", "Calgary"]};
+
+var dd = function(obj){
+
+  for (var key in obj){
+    return key + " has " + obj[key].length + " main cities ";
+};
+
+}
+
+console.log(dd({BC: ["Vancouver", "Victoria", "Prince George"],
+ AB: ["Edmonton", "Calgary"]}));
