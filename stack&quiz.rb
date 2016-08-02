bf1e190fbe1e26052d8c5f002c8612d0f4e65e8a
admin <admin@INVOKE9.local>
2016-08-02 09:27:34 -0700

    This is file for second question Stack and Queues

---
+++ stack&quiz.rb
@@ -0,0 +1,27 @@
+class Stack
+  def initialize(element)
+    @element = element
+  end
+  def add
+    @element.unshift(element)
+
+  end
+  def remove
+    @element.pop
+
+  end
+
+end
+
+class Queue
+  element
+
+  def add
+    @element.push(element)
+
+  end
+  def remove
+    @element.shift
+
+  end
+end
