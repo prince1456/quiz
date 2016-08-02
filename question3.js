27fea2db70e6258350c4adb8f4b8e8d131de1488
admin <admin@INVOKE9.local>
2016-08-02 11:09:34 -0700

    new edition

--- question3.js
+++ question3.js
@@ -1,13 +1,17 @@
-var majorCities = {BC: ["Vancouver", "Victoria", "Prince George"],
- AB: ["Edmonton", "Calgary"]};
-
-var dd = function(obj){
-
-  for (var key in obj){
-    return key + " has " + obj[key].length + " main cities ";
+var majorCities = {
+    BC: ["Vancouver", "Victoria", "Prince George"],
+    AB: ["Edmonton", "Calgary"]
 };
+
+var dd = function(obj) {
+
+    for (var key in obj) {
+        return key + " has " + obj[key].length + " main cities ";
+    };
 
 }
 
-console.log(dd({BC: ["Vancouver", "Victoria", "Prince George"],
- AB: ["Edmonton", "Calgary"]}));
+console.log(dd({
+    BC: ["Vancouver", "Victoria", "Prince George"],
+    AB: ["Edmonton", "Calgary"]
+}));
